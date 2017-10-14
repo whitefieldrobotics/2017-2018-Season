1be0b1a5b3acb7b8a64ca2e2f971a906bb48ade7
Simon <simon.mcnutt@students.whitefield.org>
2017-10-14 09:58:26 -0400

    Test,TestingAgain

--- PillarTesting.java
+++ PillarTesting.java
@@ -160,11 +160,7 @@ public class PillarTesting extends LinearOpMode {
                 }
                 sleep(50);
             }
-<<<<<<< HEAD
             //test comment
-=======
-
->>>>>>> de8ee36fda2fdcb4e73f8d609442df462d53c7f2
             arm.setPosition(.85);
             sleep(1800);
 
@@ -239,19 +235,13 @@ public class PillarTesting extends LinearOpMode {
             setRightPower(-.5);
 
             sleep(1000);
-<<<<<<< HEAD
             //test
-=======
->>>>>>> de8ee36fda2fdcb4e73f8d609442df462d53c7f2
 
             setLeftPower(0);
             setRightPower(0);
             sleep(50);
 
-<<<<<<< HEAD
 
-=======
->>>>>>> de8ee36fda2fdcb4e73f8d609442df462d53c7f2
             //drives toward pillar
             setLeftPower(.2);
             setRightPower(.2);
